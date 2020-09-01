f5dfe3f0f5ccd011044c9f677b0f186ef76afb41
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-31 23:57:01 -0400

    added store contollers amazon, ebay, walmart and members as spacesaver.html

--- controller/store-contoller.js
+++ controller/store-contoller.js
@@ -1,2 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
+const isAuthenticated = require("../config/middleware/isAuthenticated");
+
+router.get("/amazon", isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, "../views/stores/amazon.html"));
+});
+
+router.get("/ebay", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/stores/ebay.html"));
+});
+
+router.get("/walmart", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/stores/walmart.html"));
+})
+
+// router.get("/members", isAuthenticated, (req, res) => {
+//     res.sendFile(path.join(__dirname, "../views/stores/spacesaver.html"))
+// })
