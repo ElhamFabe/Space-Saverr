12cf685dd463474ec1cf46e717b5e8f667e4e2f7
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-09-01 13:57:55 -0400

    added delete, post and get routes for amazon, ebay, walmart, and spacesaver.html

--- controller/store-contoller.js
+++ controller/store-contoller.js
@@ -10,13 +10,27 @@ router.get("/amazon", isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, "../views/stores/amazon.html"));
 });
 
-// If user searches on Amazon will post in this route
+// If user searches on Amazon, search data will post in this route.
 router.post("/api/amazon", (req, res) => {
     db.create( 
-        ["name", "price", "reviews"],
+        ["name_a", "price_a", "reviews_a"],
         [req.body.name, req.body.price, req.body.reviews],
         function(result) {
             res.json({ id: result.insertId })
+    });
+});
+
+// If user deletes search on Amazon, search data will be deleted using ID on amazon table.
+router.delete("/api/amazon/:id", (req, res) => {
+    var condition = "id=" + req.params.id;
+    
+    db.delete(condition, function(result) {
+        if(result.affectedRows == 0) {
+            return res.status(404).end();
+        }
+        else {
+            res.status(200).end();
+        }
     });
 });
 
@@ -25,12 +39,84 @@ router.get("/ebay", isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, "../views/stores/ebay.html"));
 });
 
+// If user searches on eBay, search data will post in this route.
+router.post("/api/ebay", (req, res) => {
+    db.create( 
+        ["name_e", "price_e", "reviews_e"],
+        [req.body.name, req.body.price, req.body.reviews],
+        function(result) {
+            res.json({ id: result.insertId })
+    });
+});
+
+// If user deletes search on eBay, search data will be deleted using ID inside eBay table.
+router.delete("/api/ebay/:id", (req, res) => {
+    var condition = "id=" + req.params.id;
+    
+    db.delete(condition, function(result) {
+        if(result.affectedRows == 0) {
+            return res.status(404).end();
+        }
+        else {
+            res.status(200).end();
+        }
+    });
+});
+
 // User will land at walmart.html when query returns 'if user wants to see only walmart deals.' 
 router.get("/walmart", isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, "../views/stores/walmart.html"));
 })
 
-// User will land at spacesaver.html after login has been successful.
+// If user searches on Wal-Mart, search data will post in this route.
+router.post("/api/walmart", (req, res) => {
+    db.create( 
+        ["name_w", "price_w", "reviews_w"],
+        [req.body.name, req.body.price, req.body.reviews],
+        function(result) {
+            res.json({ id: result.insertId })
+    });
+});
+
+// If user deletes search on Wal-Mart, search data will be deleted using ID inside Wal-Mart table.
+router.delete("/api/walmart/:id", (req, res) => {
+    var condition = "id=" + req.params.id;
+    
+    db.delete(condition, function(result) {
+        if(result.affectedRows == 0) {
+            return res.status(404).end();
+        }
+        else {
+            res.status(200).end();
+        }
+    });
+});
+
+// User will land at spacesaver.html after login has been successful with query that will return results with deals from all pages.
 router.get("/members", isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, "../views/stores/spacesaver.html"))
 })
+
+// If user searches on spacesaver.html, search data will post in this route.
+router.post("/api/members", (req, res) => {
+    db.create( 
+        ["name_w", "price_w", "reviews_w"],
+        [req.body.name, req.body.price, req.body.reviews],
+        function(result) {
+            res.json({ id: result.insertId })
+    });
+});
+
+// If user deletes search on spacesaver.html, search data will be deleted using ID inside Wal-Mart table.
+router.delete("/api/members/:id", (req, res) => {
+    var condition = "id=" + req.params.id;
+    
+    db.delete(condition, function(result) {
+        if(result.affectedRows == 0) {
+            return res.status(404).end();
+        }
+        else {
+            res.status(200).end();
+        }
+    });
+});
