baa050f0568cd9876535f54faa140aa4377da019
Maurya Patel <mauryapatel@Chaitrangis-MacBook-Pro.local>
2020-09-03 13:09:01 -0400

    .

--- models/search.js
+++ models/search.js
@@ -1,46 +1,14 @@
-module.exports = function(sequelize, DataTypes) {
-    var Search = sequelize.define("Search", {
+module.exports = (sequelize, DataTypes) => {
+    var Search = sequelize.define("searches", {
         id: {
             type: DataTypes.INTEGER,
-            autoIncrement: true,
             primaryKey: true
         },
-        title: {
+        searchId: {
             type: DataTypes.STRING,
-            allowNull: false
-        },
-        category: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        gallery_picture: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        listing_type: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        imgUrl: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }, 
-        item_description: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        discount: {
-            type: DataTypes.DECIMAL(10,2),
-            allowNull: false
-        },
-        price: {
-            type: DataTypes.DECIMAL(10,2),
-            allowNull: false
-        },
-        ranking: {
-            type: DataTypes.DECIMAL(10,2),
             allowNull: false
         }
+        
     });
     return Search;
 };
