a0a21e76565a01cddbd4a6a5ff3a192d168c1ddb
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-09-03 11:43:48 -0400

    edited search.js and product_info.js models

--- models/search.js
+++ models/search.js
@@ -1,42 +1,14 @@
 module.exports = function(sequelize, DataTypes) {
-    var Search = sequelize.define("Search", {
+    var Search = sequelize.define("searches", {
         id: {
             type: DataTypes.INTEGER,
-            autoIncrement: true,
             primaryKey: true
         },
-        name: {
-            type: DataTypes.STRING, 
-            allowNull: false
-        }, 
-        title: {
+        searchId: {
             type: DataTypes.STRING,
-            allowNull: false
-        },
-        category: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        imgUrl: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }, 
-        item_description: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        discount: {
-            type: DataTypes.DECIMAL(10,2),
-            allowNull: false
-        },
-        price: {
-            type: DataTypes.DECIMAL(10,2),
-            allowNull: false
-        },
-        ranking: {
-            type: DataTypes.DECIMAL(10,2),
             allowNull: false
         }
+        
     });
     return Search;
 };
